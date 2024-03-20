45c036cbbb06dea2638712d1095fdde0f6cd6436
K.SRI RAM <119724502+22MH1A42G5@users.noreply.github.com>
2024-03-20 22:54:53 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Factorial/factorial.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    long long int factorial(int n){
+        //code here
+        long long int p = 1;
+        while(n!=0){
+            p*=n;
+            n--;
+        }
+        return p;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.factorial(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
