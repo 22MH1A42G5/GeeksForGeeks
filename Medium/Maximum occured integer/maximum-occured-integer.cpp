d222eb8181442525c285c6e0f461cd6d7b668ed5
K.SRI RAM <119724502+22MH1A42G5@users.noreply.github.com>
2024-06-07 22:04:08 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximum occured integer/maximum-occured-integer.cpp
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // l and r are input array
+    // maxx : maximum in r[]
+    // n: size of array
+    // arr[] : declared globally with size equal to maximum in l[] and r[]
+    // Function to find the maximum occurred integer in all ranges.
+    int maxOccured(int n, int l[], int r[], int maxx) {
+
+        // Your code here
+        vector<int>v(maxx+2,0);
+        vector<int>te;
+        for(int i = 0 ; i < n ; i++){
+            v[l[i]]++;
+            v[r[i]+1]--;
+        }
+        int s = 0,maxi = 0;
+        // for(int i = 0 ; i < v.size() ; i++){
+        //     cout<<v[i]<<" ";
+        // }
+        for(int i = 0 ; i < v.size() ; i++){
+            s+=v[i];
+            te.push_back(s);
+        }
+        // cout<<endl;
+        // for(int i = 0 ; i < te.size();i++){
+        //     cout<<te[i]<<" ";
+        // }
+        int ans;
+        for(int i = 0 ;i < v.size() ; i++){
+            if(maxi < te[i]){
+                maxi = te[i];
+                ans = i;
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+
+    // taking testcases
+    cin >> t;
+
+    while (t--) {
+        int n;
+
+        // taking size of array
+        cin >> n;
+        int l[n];
+        int r[n];
+
+        // adding elements to array L
+        for (int i = 0; i < n; i++) {
+            cin >> l[i];
+        }
+
+        int maxx = 0;
+
+        // adding elements to array R
+        for (int i = 0; i < n; i++) {
+
+            cin >> r[i];
+            if (r[i] > maxx) {
+                maxx = r[i];
+            }
+        }
+        Solution ob;
+
+        // calling maxOccured() function
+        cout << ob.maxOccured(n, l, r, maxx) << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
